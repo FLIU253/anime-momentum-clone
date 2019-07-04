6a2c686b5aa902fd129ca282bfe2c1774ace58b2
Franklin Liu <Franklin.Liu@autodatacorp.org>
2019-07-04 10:47:39 -0400

    added front page

--- new-tab-page/src/tab/App.vue
+++ new-tab-page/src/tab/App.vue
@@ -1,19 +1,53 @@
 <template>
-  <p>{{ message }}</p>
+  <div class = "container">
+    <div class="date-section">
+      <h1>{{hour}}:{{minute}}</h1>
+      <h3>Good {{message}}.</h3>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      message: "My new tab page"
+      hour: new Date().getHours(),
+      minute: new Date().getMinutes(),
+      message: ''
+    }
+  },
+  mounted(){
+    if(0 <= this.hour < 12){
+      this.message = "Morning"
+    }
+    else if( 6 > this.hour >= 12 ){
+      this.message = "Afternoon"
+    }else{
+      this.message = "Evening"
     }
   }
 }
 </script>
 
 <style scoped>
-p {
-  font-size: 20px;
+div{
+  font-family: 'Poppins', sans-serif;
+}
+.container{
+  position: relative;
+}
+.date-section{
+  font-size: 75px;
+  color: white;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  align-items: center;
+  justify-content: center;
+}
+h1, h3{
+  margin: 0;
 }
 </style>
